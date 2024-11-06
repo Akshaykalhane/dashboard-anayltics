88f63e1d8c5dffb18c82ad9a4c8e02722ef599da
Akshaykalhane <akshaykalhane1999@gmail.com>
2024-11-06 17:26:52 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
-import Image from "next/image";
+"use client"
 import styles from "./page.module.css";
+import Image from "next/image";
+
 
 export default function Home() {
+
   return (
     <div >
       <h2>App Main Page</h2>
--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,16 +1,25 @@
 "use client"
 import { usePlausible } from 'next-plausible'
+import { useRouter, useSearchParams } from 'next/navigation';
 import React, { useState } from 'react'
+
+
 
 
 function Services() {
   const [isValid,setIsValid]=useState();
   const plausible = usePlausible();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const queryParam = searchParams.get('username');
+  console.log(queryParam);
+
   const onSubmit=()=>{
     if(isValid){
       plausible("canDrive",{ props: {
         event: 'validation age',
-        eventType: 'to check validation age'
+        eventType: 'to check validation age',
+        username:queryParam
       }});
     }else{
       plausible("can'tDrive");
